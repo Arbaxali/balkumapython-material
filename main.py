b4cafbb13e6de00fa819d3c4700ce4a739b2b2bf
Arbaz ali <30619186+Arbaxali@users.noreply.github.com>
2022-10-06 15:52:25 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,80 @@
+
+
+
+
+# words =["cat","window","defenestrate"]
+# # for win in words:
+# #   print(win,len(win))
+#
+# for w in words[:]:
+#     if len(w)>6:
+#        words.insert(0,w)
+#        print(words)
+#
+
+
+
+
+# a,b=0,1
+# while a<1000:
+#   print(a,end=" ")
+#   a,b =b ,a+b
+#########################
+# i = 0
+# while i < 6:
+#   i += 1
+#   if i == 3:
+#     continue
+#   print(i)
+# i = 1
+# while i < 6:
+#   print(i)
+#   if i == 10:
+#     break
+#   i += 1
+# num1=5
+# num2=7
+# print(num1,end =" ")
+# print(num2)
+# l1=[1,2,3,4,5]
+# l2=[1,2,3,4,5]
+# # l1 = 10
+# # l2 = 10
+# print(l1 and l2)
+# print(id(l1))
+# print(id(l2))
+# vowels list
+# # vowels = [20,10,22,31,3,4]
+# #
+# # # sort the vowels
+# # vowels.sort(reverse=False)
+# #
+# # # print vowels
+# # print('Sorted list (in Descending):', vowels)
+# lst =[1,2,3,4]
+# print(lst[2])
+# print(type(1))
+# print(type([]))
+# print(type({}))
+# print(type(()))
+#
+# class balkmar:
+#     pass
+# x= balkmar()
+# print(type(x))
+#
+# ########################################################
+# #
+# # import requests
+# # import bs4
+# # import lxml
+# #
+# # res = requests.get("http://balkumartech.blogspot.com")
+# # # print(type(res))
+# # # print(res.text)
+# #
+# # soup = bs4.BeautifulSoup(res.text,"lxml")
+# # #print(soup)
+# # print(soup.select('title'))
+# # title_tag =soup.select('title')
+# # print(title_tag[0].getText())
